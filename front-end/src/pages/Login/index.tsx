9508388b4fa4d7b6f05419ec1c60d58dc452747c
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-04 00:19:51 -0400

    Build structure and style Login component

--- front-end/src/pages/Login/index.tsx
+++ front-end/src/pages/Login/index.tsx
@@ -1,8 +1,19 @@
-
+import style from "./login.module.css"
 const Login = () => {
   return (
-    <div>
-        
+    <div className={style.login}>
+        <h1 className={style.login_heading}>Real-time Chat</h1>
+        <form className={style.login_form} action={""}>
+            <div className={style.name_input}>
+                <label htmlFor="">your name</label>
+                <input type="text" />
+            </div>
+            <div className={style.room_input}>
+                <label htmlFor="">room number</label>
+                <input type="text" />
+            </div>  
+            <button>Join Room</button>
+        </form>
     </div>
   )
 }
