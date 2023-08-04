3823eacab4c9a7e078a6b6b425539dba0f76de8c
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-04 10:49:48 -0400

    Build structure and style for ChatRoom component - front-end

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,17 +2,19 @@ import { BrowserRouter } from "react-router-dom";
 import SocketProvider from "../context/SocketProvider";
 import RoomProvider from "../context/RoomProvider";
 import Router from "./pages/Router";
+import ChatRoom from "./pages/ChatRoom";
 
 function App() {
   return (
     <>
-      <SocketProvider>
+      {/* <SocketProvider>
         <RoomProvider>
           <BrowserRouter>
             <Router></Router>
           </BrowserRouter>
         </RoomProvider>
-      </SocketProvider>
+      </SocketProvider> */}
+      <ChatRoom></ChatRoom>
     </>
   );
 }
--- front-end/src/pages/ChatRoom/index.tsx
+++ front-end/src/pages/ChatRoom/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import {AiOutlineSend} from "react-icons/ai"
+import style from "./chat_room.module.css"
 const ChatRoom = () => {
   return (
-    <div>This is Chatroom page</div>
+    <div className={style.chat_room}>
+      <h1 className={style.chat_heading}>Room 2023</h1>
+      <div className={style.chat_container}>
+        <div className={`${style.message} ${style.message__me}`}>
+          <div className={style.message_content}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, blanditiis!</div>
+          <span className={`${style.message_meta}`}>{`10:00 | Robert`}</span>
+        </div>
+      </div>
+      <div className={style.chat_input}>
+        <input type="text" placeholder="Type your message..."/>
+        <span>
+          <AiOutlineSend/>
+        </span>
+      </div>
+    </div>
   )
 }
 
