3f570c7f6c5fc6c8ebb9473df4d8d6db7ac53689
DoanhTran03 <doanhtran2003@gmail.com>
2023-08-05 05:33:54 -0400

    Build functionality of RoomChat component - front-end

--- front-end/src/pages/ChatRoom/index.tsx
+++ front-end/src/pages/ChatRoom/index.tsx
@@ -9,38 +9,39 @@ const ChatRoom = () => {
   const roomContext = useRoomContext();
   const messRef = useRef<HTMLInputElement>(null);
   const [chatList, setChatList] = useState<any>([]);
-    
+
   const sendHandle = async () => {
     if (messRef.current) {
-    const data = {
-      author: roomContext?.name,
-      room: roomContext?.room,
-      message: messRef.current.value
+      const data = {
+        author: roomContext?.name,
+        room: roomContext?.room,
+        message: messRef.current.value,
+      };
+      await socketContext?.socket.emit("send_message", data);
+      setChatList((chatList: any) => [...chatList, data]);
     }
-    await socketContext?.socket.emit('send_message', data);
-    setChatList((chatList:any) => [...chatList,data]);
-  }
-  }   
-  
-  useEffect(()=>{
+  };
+
+  useEffect(() => {
     socketContext?.socket.removeAllListeners("receive_message");
-    socketContext?.socket.on('receive_message', (data) => {
-      setChatList((chatList: any) => [...chatList , data]);
-    })
-  },[socketContext?.socket])
+    socketContext?.socket.on("receive_message", (data) => {
+      setChatList((chatList: any) => [...chatList, data]);
+    });
+  }, [socketContext?.socket]);
 
   console.log(chatList);
   return (
     <div className={style.chat_room}>
-      <h1 className={style.chat_heading}>Room 2023</h1>
+      <h1 className={style.chat_heading}>{`Room: ${roomContext?.room}`}</h1>
       <div className={style.chat_container}>
-        <div className={`${style.message} ${style.message__me}`}>
-          <div className={style.message_content}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
-            blanditiis!
+        {chatList.map((data:any) => (
+          <div className={data.author === roomContext?.name ? `${style.message} ${style.message__me}` : `${style.message}`}>
+            <div className={style.message_content}>
+              {data.message}
+            </div>
+            <span className={`${style.message_meta}`}>{`10:00 | ${data.author}`}</span>
           </div>
-          <span className={`${style.message_meta}`}>{`10:00 | Robert`}</span>
-        </div>
+        ))}
       </div>
       <div className={style.chat_input}>
         <input ref={messRef} type="text" placeholder="Type your message..." />
@@ -53,3 +54,12 @@ const ChatRoom = () => {
 };
 
 export default ChatRoom;
+{
+  /* <div className={`${style.message} ${style.message__me}`}>
+          <div className={style.message_content}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+            blanditiis!
+          </div>
+          <span className={`${style.message_meta}`}>{`10:00 | Robert`}</span>
+        </div> */
+}
